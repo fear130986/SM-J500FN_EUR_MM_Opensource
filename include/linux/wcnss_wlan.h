8b11ef44e0799a102109e1562577bca7f730703c
fear130986 <fear130986@gmail.com>
2018-06-04 18:19:53 +0200

    wcnss: Add wrapper functions for init work functionalities
    WLAN prop driver cannot access INIT_WORK and INIT_DELAYED_WORK API's.
    To mitigate issue add wrapper functions in platform driver for
    INIT_WORK and INIT_DELAYED_WORK kernel APIs.
    
    CRs-Fixed: 847627
    Change-Id: Ife3c4d2abe0109fdfa0c908a3ca619cbf8b666ee
    Signed-off-by: Anand N Sunkad <asunka@codeaurora.org>

--- include/linux/wcnss_wlan.h
+++ include/linux/wcnss_wlan.h
@@ -132,6 +132,8 @@ int wcnss_ldo18_on(void);
 #endif /*CONFIG_MACH_A5U_EUR_OPEN*/
 void wcnss_flush_work(struct work_struct *work);
 void wcnss_flush_delayed_work(struct delayed_work *dwork);
+void wcnss_init_work(struct work_struct *work , void *callbackptr);
+void wcnss_init_delayed_work(struct delayed_work *dwork , void *callbackptr);
 int wcnss_get_iris_name(char *iris_version);
 void wcnss_en_wlan_led_trigger(void);
 void wcnss_dump_stack(struct task_struct *task);
